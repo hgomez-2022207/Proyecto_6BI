b2b127a3167d709547f16f848ee2fd3181562d45
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-18 20:18:02 -0600

    Se crea agreagar productos

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -2,6 +2,20 @@ const bcryptjs = require('bcryptjs');
 const Producto = require('../models/productos');
 const { response } = require('express');
 
-module.exports = {
+const productoPost = async (req,res) => {
+    console.log('productopost')
+    const {nombre,precio,cantidad,empresa,descripcion,categoria} = req.body;
+    const producto = new Producto({nombre,precio,cantidad,empresa,descripcion,categoria});
 
+    //const salt = bcryptjs.genSaltSync();
+
+    await producto.save();
+
+    res.status(200).json({
+        producto
+    });
+}
+
+module.exports = {
+    productoPost
 }
--- routers/productos.routes.js
+++ routers/productos.routes.js
@@ -1,8 +1,22 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
-
-const {} = require('../controllers/producto.controller');
+const { validarCampos, esAdminRole, tieneRolAutorizado } = require('../middlewares/validar-campos');
+const {productoPost} = require('../controllers/producto.controller');
 
 const router = Router();
+
+router.post(
+    "/",
+    [
+        check("nombre","El nombre del producto").not().isEmpty(),
+        check('precio','El precio de producto debe ser registrado').not().isEmpty(),
+        check('cantidad','Acaso no tenemos existencias del producto?').not().isEmpty(),
+        check('empresa','Los creadores del producto').not().isEmpty(),
+        check('descripcion','Como le expicaremos a los clientes cual es la funcion del producto').not().isEmpty(),
+        check('categoria','Nos da una idea del funcionamiento del producto').not().isEmpty(),
+        validarCampos
+        
+    ], productoPost
+);  
 
 module.exports = router;
