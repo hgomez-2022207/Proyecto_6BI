3b3615b24de0636fff8eb2b80d8503516fe1dd45
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-18 22:00:31 -0600

    Se crea post en categoriaController

--- controllers/categoria.controller.js
+++ controllers/categoria.controller.js
@@ -2,6 +2,18 @@ const bcryptjs = require('bcryptjs');
 const Categoria = require('../models/categorias');
 const { response } = require('express');
 
-module.exports = {
+const categoriaPost = async(req,res) => {
+    console.log('categoria');
+    const {nombreCategoria, edad} = req.body;
+    const categoria = new Categoria({nombreCategoria,edad});
 
+    await categoria.save();
+
+    res.status(200).json({
+        categoria
+    });
+}
+
+module.exports = {
+    categoriaPost,
 }
