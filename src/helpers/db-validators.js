33d48eace8e1fb5a222e58e9db26e441df3b16d1
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-04 16:44:47 -0600

    Se restructura validators.js

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,39 +1,30 @@
-const { Curso } = require('mongoose');
-const Role = require('../models/role');
-const Usuario = require('../models/usuario');
+import Role from '../role/role.js';
+import Usuario from '../usuario/usuario.js';
 
-const esRoleValido = async (role = '') => {
+export const esRoleValido = async (role = '') => {
     const existeRol = await Role.findOne({role});
     if(!existeRol){
         throw new Error(`El role ${ role } no existe en la base de datos`);
     }
 }
 
-const existeEmail = async (correo = '') => {
+export const existeEmail = async (correo = '') => {
     const existeEmail = await Usuario.findOne({correo});
     if(existeEmail){
         throw new Error(`El usuario con el ${ id } no existe`);
     }
 }
 
-const existeUsuarioById = async (id = '') =>{
+export const existeUsuarioById = async (id = '') =>{
     const existeusuario = await Usuario.findOne({id});
     if(existeusuario){
         throw new Error(`El usuario no existe`)
     }
 }
 
-const existeProductoById = async (id = '') =>{
+export const existeProductoById = async (id = '') =>{
     const existeProducto = await Producto.findOne({id});
     if(existeProducto){
         throw new Error(`El producto no existe`)
     }
-}
-
-
-module.exports = {
-    esRoleValido,
-    existeEmail,
-    existeUsuarioById,
-    existeProductoById
 }
