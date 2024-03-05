5c42d0c61036c0069066429cef5e280bdb8b8406
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-05 16:13:14 -0600

    Se a;ade en route el actualizar categoria

--- src/categoria/categoria.routes.js
+++ src/categoria/categoria.routes.js
@@ -1,7 +1,7 @@
 import {Router} from 'express';
 import {check} from 'express-validator';
 
-import {categoryPost} from './categoria.controller.js';
+import {categoryPost, categoryPut} from './categoria.controller.js';
 import { validarCampos } from '../middlewares/validar-campos.js';
 import {existeCategoria} from '../helpers/db-validators.js';
 
@@ -14,6 +14,14 @@ router.post("/",
         check("categoria").custom(existeCategoria),
         validarCampos
     ],categoryPost
+);
+
+router.put("/",
+    [
+        check("newcategoria","El nombre de la categoria es obligatorio").not().isEmpty(),
+        check("edad","Es para especificar una edad adecuada par la categoria del producto").not().isEmpty(),
+        //check("newcategoria").custom(existeCategoria),
+    ],categoryPut
 );
 
 export default router;
