b695173ae08bc6e239ba4eae5e68c83e44f67857
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-05 16:12:31 -0600

    Se crea el metodo de actualizar categoria

--- src/categoria/categoria.controller.js
+++ src/categoria/categoria.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import Categoria from '../categoria/categorias.js';
+import Producto from '../producto/productos.model.js'
 import  response  from  'express';
 
 export const categoryPost = async (req,res) => {
@@ -12,5 +13,41 @@ export const categoryPost = async (req,res) => {
     res.status(200).json({
         categoria,
         cat
+    });
+}
+
+export const categoryPut = async (req,res) => {
+    console.log('categoryPut');
+    const {categoria} = req.query;
+    const {newcategoria,edad} = req.body;
+
+    const cat = await Categoria.findOne({categoria})
+    const p = await Producto.findOne({categoria})
+
+    if(!cat){
+        return res.status(400).json({
+            msg: "Categoria no existe, vuelva a ingresarla en el query"
+        });
+    }/*
+    if(!p){
+        return res.status(400).json({
+            msg: "Categoria no existe, vuelva a ingresarla en el query cccccccccccccc"
+        });
+    }*/
+
+    cat.categoria=newcategoria;
+    cat.edad=edad;
+    p.categoria=newcategoria;
+
+    const pcategory = await Producto.findByIdAndUpdate(p.id,p)
+    const category = await Categoria.findByIdAndUpdate(cat.id, cat);
+
+    res.status(200).json({
+        msg: "Datos actualizados",
+        category,
+        categoria,
+        edad,
+        msg: `Cambio a productos`,
+        pcategory
     });
 }
