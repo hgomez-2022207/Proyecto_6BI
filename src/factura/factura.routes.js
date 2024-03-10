0b7b51b696737046e406f69df6bfa26b5378a313
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-10 02:14:52 -0600

    Se crean lo metodos de buscar factura y buscar por correo

--- src/factura/factura.routes.js
+++ src/factura/factura.routes.js
@@ -1,8 +1,30 @@
-const {Router} = require('express');
-const {check} = require('express-validator');
-
-const {} = require('./factura.controllers');
+import {Router} from 'express';
+import {check} from 'express-validator';
+import {facturaPost, getFacturaByEmail} from "./factura.controllers.js"
+import { validarCampos } from '../middlewares/validar-campos.js';
+import { validarJWT } from '../middlewares/validar-jwt.js';
+import { tieneRolAutorizado } from '../middlewares/validar-roles.js';
+import {existeEmail} from '../helpers/db-validators.js';
 
 const router = Router();
 
-module.exports = router;
+router.post(
+    '/',
+    [
+        check("fecha","Fecha de la compra").not().isEmpty(),
+        check("correo","El correo del cliente").not().isEmpty(),
+        check("metodoP","El metodo con el que va a pagar el cliente").not().isEmpty(),
+        check("correo").custom(existeEmail),
+        validarCampos,
+        validarJWT,
+        tieneRolAutorizado('ADMIN_ROLE')
+    ],facturaPost
+);
+
+router.get(
+    '/',
+    [validarJWT],
+    getFacturaByEmail
+)
+
+export default router;
