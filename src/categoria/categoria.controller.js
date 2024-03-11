e85f71188d63c0509547083ab69c5ce6d20414c5
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-10 20:20:31 -0600

    Se elimina codigo innecesario

--- src/categoria/categoria.controller.js
+++ src/categoria/categoria.controller.js
@@ -33,50 +33,6 @@ export const getCategoria = async (req,res) => {
     });
 }
 
-/*export const categoryPut = async (req,res) => {
-    console.log('categoryPut');
-    const {categoria} = req.query;
-    const {newcategoria,edad} = req.body;
-
-    const cat = await Categoria.findOne({categoria})
-    const p = await Producto.findOne({categoria})
-
-    if(!cat){
-        return res.status(400).json({
-            msg: "Categoria no existe, vuelva a ingresarla en el query"
-        });
-    }
-    if(!p){
-        cat.categoria=newcategoria;
-        cat.edad=edad;
-        const category = await Categoria.findByIdAndUpdate(cat.id, cat);
-        return res.status(400).json({
-            newcategoria,
-            edad,
-            msg: "Categoria no esta registrada en productos, por lo que no se ve afectado"
-        });
-    }
-
-    cat.categoria=newcategoria;
-    cat.edad=edad;
-    p.categoria=newcategoria;
-
-    const pcategory = await Producto.findByIdAndUpdate(p.id,p)
-    const category = await Categoria.findByIdAndUpdate(cat.id, cat);
-
-    res.status(200).json({
-        msg: "Datos actualizados",
-        category,
-        categoria,
-        edad,
-        msg:'despues:',
-        categoria,
-        edad,
-        msg: `Cambio a productos`,
-        pcategory
-    });
-}*/
-
 export const categoryPut = async (req, res) => {
     console.log('categoryPut');
     const { categoria } = req.query;
